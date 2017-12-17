f87d0184821790ffc178654bea00f7a7fb2448d7
CherishClark <cherish.g.clark@gmail.com>
2017-12-16 17:55:00 -0700

    secrets

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+
+  get 'pages/home'
+
+  get 'pages/contact'
+
+  get 'pages/about'
+
+  get 'pages/shop'
+
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
