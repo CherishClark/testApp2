c3372347bc05c38e1deb6796a8d2a248bfb0204e
CherishClark <cherish.g.clark@gmail.com>
2017-12-16 18:10:44 -0700

    created pages and installed devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
 
-  get 'pages/home'
 
-  get 'pages/contact'
 
-  get 'pages/about'
+  devise_for :users
+  get 'contact', to: 'pages#contact'
 
-  get 'pages/shop'
+  get 'about', to: 'pages#about'
+
+  get 'shop', to: 'pages#shop'
+
+  root to: 'pages#home'
 
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
